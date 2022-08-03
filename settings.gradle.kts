429132e87d0d4438b4c0fdfb96280b16b98a51cf
Marcel Richter <mail@mrclrchtr.de>
2022-08-03 17:09:49 +0200

    Add build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,19 @@
+plugins {
+    id("com.gradle.enterprise") version("3.10.3")
+}
+
 rootProject.name = "gradle-kotlin-spring"
 include(":message-factory")
 include(":message-dashboard")
 
 enableFeaturePreview("VERSION_CATALOGS")
+
+if (!System.getenv("CI").isNullOrEmpty() && !System.getenv("BUILD_SCAN_TOS_ACCEPTED").isNullOrEmpty()) {
+    gradleEnterprise {
+        buildScan {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+            tag("CI")
+        }
+    }
+}
