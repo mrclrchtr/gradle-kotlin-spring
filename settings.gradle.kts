a57c81a27ed835dfc65b17f23351d8064b4a850e
Marcel Richter <mail@mrclrchtr.de>
2022-08-03 17:57:18 +0200

    Removed deprecated enableFeaturePreview("VERSION_CATALOGS")

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,6 @@ rootProject.name = "gradle-kotlin-spring"
 include(":message-factory")
 include(":message-dashboard")
 
-enableFeaturePreview("VERSION_CATALOGS")
-
 if (!System.getenv("CI").isNullOrEmpty() && !System.getenv("BUILD_SCAN_TOS_ACCEPTED").isNullOrEmpty()) {
     gradleEnterprise {
         buildScan {
